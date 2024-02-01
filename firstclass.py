8d2e54acef555c57b3697e77d19a137a9a080992
kusiwaadanso <125768431+kusiwaadanso@users.noreply.github.com>
2024-02-01 12:49:14 +0000

    Update firstclass.py

--- firstclass.py
+++ firstclass.py
@@ -1,15 +1,32 @@
-from math import sqrt
+#delta determines if roots will be real or not
 
-a,b,c=eval(input('Enter coefficients'))
 
-disc=b*b-4*a*c
+#function for delta
+def delta(a,b,c):
+    delta=b**2-4*a*c
+    return delta
+   
+#function for root extration
 
-if disc>0:
-    x1=(-b+sqrt(disc))/2*a
-    x2=(-b+sqrt(disc))/2*a
-    print(f'Solution: {x1},{x2}')
+def root_extraction(a,b,delta):
+    if delta<0:
+        return 'the equation has no real roots'
+    elif delta==0:
+        x1=(b**2-delta)/(2*a)
+       return x
+elif delta>0:
+x1=(-b-delta**(0.5))/(2*a)
+x2=(-b+delta**(0.5))/(2*a)
+return (x1,x2)
 
-else:
-    print('No solution')
+
+
+
+
+
+
+
+
+
 
 
